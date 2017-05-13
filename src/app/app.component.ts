5a319f72f95ec025e072c6f541d5b05a1cca8ed6
Denis Michailov <kamrad.reg@gmail.com>
2017-05-13 19:32:48 +1000

    Added new example. Lighting entered number animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+
   animations: [
     trigger('clickedState', [
       state('default', style({
@@ -13,19 +14,61 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         height: '100px'
       })),
       state('clicked', style({
-        backgroundColor: 'lightblue',
+        backgroundColor: 'orange',
         width: '100%',
         height: '100px'
       })),
+      state('mousedown', style({
+        backgroundColor: 'red'
+      })),
       transition('default => clicked', animate('200ms 100ms ease-in')),
-      transition('clicked => default', animate('400ms 100ms ease-out'))
+      transition('clicked => default', animate('400ms 100ms ease-out')),
+      transition('mousedown <=> default', animate('400ms 100ms ease-out')),
+      transition('mousedown <=> clicked', animate('400ms 100ms ease-out'))
+    ]),
+    trigger('numberEnteredState', [
+      state('unselected', style({
+        border: '1px solid #333',
+        padding: '5px',
+        backgroundColor: 'white',
+        color: '#333'
+      })),
+      state('selected', style({
+        border: '2px solid red',
+        padding: '4px',
+        backgroundColor: '#333',
+        color: 'white'
+      })),
+      transition('unselected => selected', [
+        style({
+          border: '2px solid #333',
+          padding: '4px'
+        }),
+        animate(2000, style({
+          backgroundColor: 'red'
+        })),
+        animate(2000)
+      ])
     ])
   ]
+
 })
 export class AppComponent {
   clickInfo = 'default';
+  simpleState = 'default';
+  paragraphInfo = 'default';
+  numberEntered;
 
   onClickSimple() {
-    this.clickInfo == 'default' ? this.clickInfo = 'clicked' : this.clickInfo = 'default'
+    this.simpleState == 'default' ? this.simpleState = 'clicked' : this.simpleState = 'default';
+    this.clickInfo = this.simpleState;
+  }
+
+  onMousedownSimple() {
+    this.clickInfo = 'mousedown';
+  }
+
+  onClickParagraph() {
+    this.paragraphInfo == 'default' ? this.paragraphInfo = 'clicked' : this.paragraphInfo = 'default';
   }
 }
