1b0be13c6d1e431bb50ca1e7d5f36f5380ef0b69
Denis Michailov <kamrad.reg@gmail.com>
2017-05-13 16:47:52 +1000

    Added clickedState trigger with two state. Added simple click interaction - changing state between default and clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedState', [
+      state('default', style({
+        backgroundColor: 'orange',
+        width: '100px',
+        height: '100px'
+      })),
+      state('clicked', style({
+        backgroundColor: 'lightblue',
+        width: '300px',
+        height: '50px'
+      })),
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  clickInfo = 'default';
+
+  onClickSimple() {
+    this.clickInfo == 'default' ? this.clickInfo = 'clicked' : this.clickInfo = 'default'
+  }
 }
